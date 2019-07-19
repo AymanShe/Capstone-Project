da1962e53a7e6fd6bb3606719767218e57d007c4
Ayman <aymalsh@gmail.com>
2019-07-20 00:41:54 +0300

    implement AdMob
    implement: implement AdMob and add AdView in the layout.
    
    To monetize the app, AdMob is added. an AdView is added in the layout and an ad is requested and loaded into the view in the activity. include the Unit Id in the manifest for the app to work properly.

--- app/src/main/java/com/aymanshehri/whenimthere/MainActivity.java
+++ app/src/main/java/com/aymanshehri/whenimthere/MainActivity.java
@@ -3,11 +3,31 @@ package com.aymanshehri.whenimthere;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //region AdMob
+        //code to load ads
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        adView.loadAd(adRequest);
+        //endregion
+
+
+
+        myRef.setValue("Hello, World!");
     }
 }
