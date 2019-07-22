a6126ee690d81467b5c2d88024cc81d24969fa25
Ayman <aymalsh@gmail.com>
2019-07-22 04:34:20 +0300

    Implement swipe background color and icon effect

--- app/src/main/java/com/aymanshehri/whenimthere/ListFragment.java
+++ app/src/main/java/com/aymanshehri/whenimthere/ListFragment.java
@@ -1,6 +1,10 @@
 package com.aymanshehri.whenimthere;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +12,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -27,13 +32,13 @@ public class ListFragment extends Fragment {
     private FirebaseFirestore dbRef = FirebaseFirestore.getInstance();
     private CollectionReference itemRef = dbRef.collection("items");
 
-    String isGotListKey = "isGotListKey";
+    private boolean isGotList;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_items, container,false);
+        View view = inflater.inflate(R.layout.fragment_items, container, false);
         FloatingActionButton addItemButton = view.findViewById(R.id.fab_add_item);
         addItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,8 +47,9 @@ public class ListFragment extends Fragment {
             }
         });
 
-        boolean isGotList = false;
+        isGotList = false;
         Bundle passedBundle = getArguments();
+        String isGotListKey = "isGotListKey";
         if (passedBundle != null)
             isGotList = passedBundle.getBoolean(isGotListKey);
         boolean test = isGotList;
@@ -61,7 +67,18 @@ public class ListFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
 
-        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            
+            int iconResourceId;
+            Drawable icon;
+            int intrinsicWidth;
+            int intrinsicHeight;
+            int iconTop;
+            int iconMargin;
+            int iconLeft;
+            int iconRight;
+            int iconBottom;
+
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
                 return false;
@@ -71,12 +88,82 @@ public class ListFragment extends Fragment {
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
                 if (direction == ItemTouchHelper.LEFT) {
                     adapter.deleteItem(viewHolder.getAdapterPosition());
-                }else{
+                } else {
                     adapter.toggleStatus(viewHolder.getAdapterPosition());
                 }
 
             }
-        }).attachToRecyclerView(recyclerView);
+
+            //The swipe to delete color effect is taken from https://medium.com/@kitek/recyclerview-swipe-to-delete-easier-than-you-thought-cff67ff5e5f6
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                View itemView = viewHolder.itemView;
+                int itemHeight = itemView.getBottom() - itemView.getTop();
+
+                ColorDrawable background = new ColorDrawable();
+
+                if (dX < 0) {
+                    // Draw the red background on the canvas
+                    String swipeLeftColor = "#f44336";
+                    background.setColor(Color.parseColor(swipeLeftColor));
+                    background.setBounds(itemView.getRight() + Math.round(dX), itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                    background.draw(c);
+
+                    // Declare the delete icon
+                    iconResourceId = R.drawable.ic_delete;
+                    icon = ContextCompat.getDrawable(getContext(), iconResourceId);
+                    intrinsicWidth = icon.getIntrinsicWidth();
+                    intrinsicHeight = icon.getIntrinsicHeight();
+
+                    // Calculate position of delete icon
+                    iconTop = itemView.getTop() + (itemHeight - intrinsicHeight) / 2;
+                    iconMargin = (itemHeight - intrinsicHeight) / 2;
+                    iconLeft = itemView.getRight() - iconMargin - intrinsicWidth;
+                    iconRight = itemView.getRight() - iconMargin;
+                    iconBottom = iconTop + intrinsicHeight;
+
+                    // Draw the delete icon on the canvas
+                    icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+                    icon.draw(c);
+                } else {
+                    // Draw the red background on the canvas
+                    String swipeRightColor;
+                    if (isGotList)
+                        swipeRightColor = "#F49136";
+                    else
+                        swipeRightColor = "#29BB43";
+                    background.setColor(Color.parseColor(swipeRightColor));
+                    background.setBounds(itemView.getLeft(), itemView.getTop(), itemView.getLeft() + Math.round(dX), itemView.getBottom());
+                    background.draw(c);
+
+                    // Declare the check icon
+                    if (isGotList)
+                        iconResourceId = R.drawable.ic_return;
+                    else
+                        iconResourceId = R.drawable.ic_check;
+                    icon = ContextCompat.getDrawable(getContext(), iconResourceId);
+                    intrinsicWidth = icon.getIntrinsicWidth();
+                    intrinsicHeight = icon.getIntrinsicHeight();
+
+                    // Calculate position of check icon
+                    iconTop = itemView.getTop() + (itemHeight - intrinsicHeight) / 2;
+                    iconMargin = (itemHeight - intrinsicHeight) / 2;
+                    iconLeft = itemView.getLeft() + iconMargin;
+                    iconRight = itemView.getLeft() + iconMargin + intrinsicWidth;
+                    iconBottom = iconTop + intrinsicHeight;
+
+                    // Draw the delete icon on the canvas
+                    icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+                    icon.draw(c);
+                }
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+            }
+        });
+
+        itemTouchHelper.attachToRecyclerView(recyclerView);
 
         return view;
     }
